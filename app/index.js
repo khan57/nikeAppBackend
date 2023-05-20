1b935b232ae4fa4e4042d0477cfef9c34508981c
Haseeb ur rehman <haseeb.ktk57@gmail.com>
2023-05-20 12:21:12 +0500

    logger added

--- app/index.js
+++ app/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
 const PORT = 3000;
 
 app.use(express.json({}));
 app.use(express.urlencoded({ extended: true }));
+
+app.use(morgan());
 
 app.get("/", (req, res) => {
   return res.json({ msg: "Welcome to Nike App APIS" });
